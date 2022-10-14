d9459ec1bb81ea112cf5727e28e7aa147251f24f
hataerin <goil1113@likelion.org>
2022-10-14 14:58:56 +0900

    ADD: init
    Signed-off-by: hataerin <goil1113@likelion.org>

---
+++ src/main/java/com/goorm/baromukja/BaromukjaApplication.java
@@ -0,0 +1,13 @@
+package com.goorm.baromukja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaromukjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaromukjaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goorm/baromukja/BaromukjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goorm.baromukja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaromukjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
