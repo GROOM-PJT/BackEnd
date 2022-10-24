3c52d562ce9858dc944429623cb5d89ab8690625
Jeeseob <jeeseob5761@gmail.com>
2022-10-24 15:30:27 +0900

    UPDATE: Paging 추가\

---
+++ src/main/java/com/goorm/baromukja/dto/ListRequest.java
@@ -0,0 +1,14 @@
+package com.goorm.baromukja.dto;
+
+import lombok.Getter;
+
+/**
+ * @Author : Jeeseob
+ * @CreateAt : 2022/10/24
+ */
+
+@Getter
+public class ListRequest {
+    private int skip;
+    private int limit;
+}
